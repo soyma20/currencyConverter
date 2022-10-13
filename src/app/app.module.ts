38ccd6cb735be4b320adcab6c138dd9358e1e471
soyma20 <slaviksoyma2014@gmail.com>
2022-10-13 17:00:38 +0300

    feat: add wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {CurrenciesInputsComponent} from './components/currencies-inputs/currencies-inputs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrenciesInputsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
